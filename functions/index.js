3e27eb41cd7c527634d19ffefc80609a1c8c53e3
institutofernandoantonio-art <institutofernandoantonio@gmail.com>
2026-09-15 17:56:58 -0300

    feat: Adicionado suporte ao Firebase Cloud Functions

---
+++ functions/index.js
@@ -0,0 +1,9 @@
+const { onRequest } = require("firebase-functions/v2/https");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+// Endpoint de teste
+exports.ping = onRequest((req, res) => {
+  res.json({ ok: true, message: "Firebase Functions do IFA funcionando!" });
+});
